5b0d650a193063afd72df1d72ac6950c34e394aa
root <root@fuel.domain.tld>
2016-07-07 08:43:20 +0000

    Add check that uptated pkgs were installed from configured repos

--- playbooks/files/pack_diff.sh
+++ playbooks/files/pack_diff.sh
@@ -3,11 +3,11 @@
 PACK=${1?:"Please specify package name."}
 APT_CONF=${2?:"Please specify apt config file."}
 
-VERS=$(apt-cache policy ${PACK}| awk '/Installed/ {print $2}')
-VERS=${VERS/\:/\%3a}
+VERS_ORIG=$(apt-cache policy ${PACK}| awk '/Installed/ {print $2}')
+VERS=${VERS_ORIG/\:/\%3a}
 
-NEW_VERS=$(apt-cache policy ${PACK} | awk '/Candidate/ {print $2}')
-NEW_VERS=${NEW_VERS/\:/\%3a}
+NEW_VERS_ORIG=$(apt-cache policy ${PACK} | awk '/Candidate/ {print $2}')
+NEW_VERS=${NEW_VERS_ORIG/\:/\%3a}
 
 PYTHON_PACK=${PACK##python-}
 PACK_NAME="${PACK}_${VERS}_all.deb"
@@ -20,14 +20,23 @@ INSTALLED_PACK="/usr/lib/python2.7/dist-packages/${PYTHON_PACK}"
 DIFF="${EXTRACTED_PACK}/${PACK}_customization.patch"
 
 mkdir -p ${EXTRACTED_PACK}/${VERS}         &&
-  cd ${EXTRACTED_PACK}/${VERS}             || exit 1
-ar p ${CACHED_PACK} data.tar.xz | tar xJ   || exit 1
+  cd ${EXTRACTED_PACK}/${VERS}             &&
+  rm -rf *                                 || exit 1
+
+if ! [ -e ${CACHED_PACK} ]
+then
+  apt-get -c ${APT_CONF} download ${PACK}=${VERS_ORIG} || exit 1
+  CACHED_PACK=${PACK_NAME}
+fi
+ar p ${CACHED_PACK} data.tar.xz | tar xJ
 diff -c -r -x "*.pyc" ./${INSTALLED_PACK} ${INSTALLED_PACK} > ${DIFF}
 
 mkdir -p ${EXTRACTED_PACK}/${NEW_VERS}     &&
-  cd ${EXTRACTED_PACK}/${NEW_VERS}         || exit 1
+  cd ${EXTRACTED_PACK}/${NEW_VERS}         &&
+  rm -rf *                                 || exit 1
 apt-get -c ${APT_CONF} download ${PACK}
 ar p ${NEW_PACK_NAME} data.tar.xz | tar xJ || exit 1
 
 cd ${EXTRACTED_PACK}
 patch -p1 -N --dry-run --silent -d ${NEW_VERS} < ${DIFF}
+
---
+++ playbooks/files/packages-verify-ubuntu.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+function md5_verify()
+{
+    PGK=${1:?"Please specify package name."}
+    if [ -f "/var/lib/dpkg/info/${PKG}:amd64.md5sums" ]
+    then
+        result="$(cd /; nice -n 19 ionice -c 3 md5sum --quiet -c /var/lib/dpkg/info/${PKG}:amd64.md5sums 2>&1)"
+    else
+        result="$(cd /; nice -n 19 ionice -c 3 md5sum --quiet -c /var/lib/dpkg/info/${PKG}.md5sums 2>&1)"
+    fi
+
+    test $? -eq 0 && return 0
+
+    #exclude packages for which we cannot verify md5sum, no md5sums file
+    if [ "$(echo "$result" | grep md5sums | grep -c 'No such file')" -gt 0 ]
+    then
+        return 3
+    fi
+
+    #exclude ./etc (elasticsearch packaging issue)
+    result="$(echo "$result" | grep -v '^\./etc/')"
+
+    #exclude .pyc files
+    result="$(echo "$result" | grep -v '\.pyc$')"
+
+    #exclude bad formatted / empty md5sum files and md5sum summary lines
+    result="$(echo "$result" | grep -v 'did NOT match\|md5sum: ')"
+    if [ -n "$result" ]
+    then
+        #echo -e "$result" | sed "s|^|$l\t|"
+        return 1
+    fi
+    return 2
+}
+
+APT_CONF=${1:-"/etc/apt/apt.conf"}
+
+CUSTOM_PACKAGES=""
+RET=0
+for PKG in $(apt-get --just-print upgrade | awk '/Conf/ {print $2}' ); do
+    apt-cache -c ${APT_CONF} policy ${PKG} | grep "\*\*\*" -A1 | grep Packages &> /dev/null
+    if (( $? != 0 ))
+    then
+        echo "Packet ${PKG} was installed not from the configured repositories."
+        RET=1
+        continue
+    fi
+    md5_verify ${PKG}
+    if (( $? == 1 ))
+    then
+        echo ${PKG}
+    fi
+done
+#test ${RET} && echo -e "${CUSTOM_PACKAGES}"
+exit ${RET}
+
