fa128376ab02f4b395b42fa919121044b19c2b5d
Andrey Epifanov <aepifanov@mirantis.com>
2016-08-05 04:06:28 +0000

    Add verifying customizations

--- playbooks/files/verify_customizations_consistency.sh
+++ playbooks/files/verify_customizations_consistency.sh
@@ -1,28 +1,101 @@
 #!/bin/bash
 
+FUEL_CUSTOM_DIR=${1:-$FUEL_CUSTOM_DIR}
 FUEL_CUSTOM_DIR=${FUEL_CUSTOM_DIR:?"FUEL_CUSTOM_DIR is undefined!"}
+FUEL_PATCHES_DIR=${2:-$FUEL_PATCHES_DIR}
 FUEL_PATCHES_DIR=${FUEL_PATCHES_DIR:?"FUEL_PATCHES_DIR is undefined!"}
 
-[ -d "$FUEL_CUSTOM_DIR" ] && PATCHES="$(find $FUEL_CUSTOM_DIR -name '*.patch' | rev | cut -d'/' -f1 | rev | sort -u)" || exit 0
-FAIL=0
-for PATCH in $PATCHES
-do
-    FILES="$(find $FUEL_CUSTOM_DIR -name "$PATCH")"
-    for FILE in $FILES
-    do
-        MD5="$(grep -v -e '^diff' -e '^---' -e '^+++' "$FILE" | md5sum)"
-        FILES_MD5+=$MD5$FILE$'\n'
+PROCESSED_DIR="processed"
+FUEL_PROCESSED_DIR="${FUEL_CUSTOM_DIR}/${PROCESSED_DIR}"
+FUEL_UNIQUE_DIR="${FUEL_PROCESSED_DIR}/unique"
+FUEL_RESULT="${FUEL_PROCESSED_DIR}/result.txt"
+
+cd "${FUEL_CUSTOM_DIR}" || exit 0
+ALL_NODES=$(ls | grep -v "${PROCESSED_DIR}" | sort)
+
+mkdir -p "${FUEL_PROCESSED_DIR}"
+rm -rf "${FUEL_PROCESSED_DIR}"/*
+
+for NODE in ${ALL_NODES}; do
+    cd ${FUEL_CUSTOM_DIR}/${NODE}
+    for PATCH in *.patch; do
+        [ -e "${PATCH}" ] || continue
+        PKG=${PATCH%%_*.patch}
+        mkdir -p "${FUEL_PROCESSED_DIR}/${PKG}/${NODE}"
+        cp "${PATCH}" "${FUEL_PROCESSED_DIR}/${PKG}/${NODE}"
     done
-    UNIQUE_MD5=$(echo "$FILES_MD5" | cut -d' ' -f1 | sort -u | grep -v '^$' | wc -l)
-    if [ "$UNIQUE_MD5" -gt 1 ]
-    then
-        echo "Found inconsistent patch $PATCH. This patch differs across nodes."
-        echo "$FILES_MD5"
-        FAIL=1
-    else
-        echo "$FILE"
-        [ ! -d "$FUEL_PATCHES_DIR" ] && mkdir -p $FUEL_PATCHES_DIR
-        cp $FILE $FUEL_PATCHES_DIR
-    fi
 done
-[ "$FAIL" = 0 ] || exit 1
+
+cd "${FUEL_PROCESSED_DIR}" || exit 0
+ALL_PKGS=$(ls | sort)
+
+
+MD5_ARRAY=()
+get_array_id_for_md5()
+{
+    local i=0
+    MD5=${1:?"No MD5"}
+    ARRAY_SIZE=${#MD5_ARRAY[@]}
+    for ((i=0; i<${ARRAY_SIZE}; i++)); do
+        if [ $MD5 == ${MD5_ARRAY[$i]} ]; then
+            return $i
+        fi
+    done
+    MD5_ARRAY[$i]=${MD5}
+    return $i
+}
+
+RET=0
+set_return_code()
+{
+    NEW_RET=$1
+    [ -z "${NEW_RET}" ] && return 0
+    (( "${NEW_RET}" > "${RET}")) && { RET="${NEW_RET}"; }
+}
+
+ARRAY=()
+imax=$(echo ${ALL_PKGS} | wc -w)
+jmax=$(echo ${ALL_NODES} | wc -w)
+i=0 #PKG
+for PKG in ${ALL_PKGS}; do
+    MD5_ARRAY=()
+    cd "${FUEL_PROCESSED_DIR}/${PKG}"
+    j=0 #NODE
+    for NODE in ${ALL_NODES}; do
+        if [ ! -d ${NODE} ]; then
+            ARRAY[$i+$j*$imax]="-"
+            set_return_code 1
+        else
+            FILE_PATCH="${NODE}/${PKG}_customization.patch"
+            MD5="$(grep -v -e '^diff' -e '^---' -e '^+++' "${FILE_PATCH}" | md5sum | awk '{print $1}')"
+            get_array_id_for_md5 ${MD5}
+            ID=$?
+            ARRAY[$i+$j*$imax]=${ID}
+        fi
+        ((j++))
+    done
+    if (( ${#MD5_ARRAY[*]} > 1 )); then
+        set_return_code 2
+    fi
+    ((i++))
+done
+
+OUT="nodes/packages $(echo ${ALL_PKGS})\n"
+j=0 #NODE
+for NODE in ${ALL_NODES}; do
+    LINE="${NODE}"
+    i=0 #PKG
+    for PKG in ${ALL_PKGS}; do
+        LINE+=" ${ARRAY[$i+$j*$imax]}"
+        ((i++))
+    done
+    ((j++))
+    OUT+="${LINE}\n"
+done
+
+echo -e "Legenda:
+ '-' - no patch(customizations) for package on this node
+ 'x' - ID of patch\n"
+echo -e "${OUT}" | column -t
+
+exit ${RET}
--- playbooks/files/verify_packages_ubuntu.sh
+++ playbooks/files/verify_packages_ubuntu.sh
@@ -1,87 +1,69 @@
 #!/bin/bash
 
-# This script
-# - make
-#     apt-get -c $1 update
-# - for each upgradable package verify md5 sum
-# - return:
+# This script verifies MD5 sum for all installed python packages
+# Return:
 #     0 - ok
 #     1 - some packets were installed not from configured repositories
 #     2 - some other error
-# - output:
-#     stdout - list of customized packages and undentified packages
-#     stderror - list of customized non-python packages
-
+# Output:
+#     List of customized packages and unidentified packages
 APT_CONF=${APT_CONF:-$1}
 APT_CONF=${APT_CONF:-"/etc/apt/apt.conf"}
 
-
+# Return:
+#     0 - ok
+#     1 - installed package was customized
+#     x - some other error
 function md5_verify()
 {
     PKG=${1:?"Please specify package name."}
-
     cd / || return 2
 
-    if [ -f "/var/lib/dpkg/info/${PKG}:amd64.md5sums" ]
-    then
-        result="$(nice -n 19 ionice -c 3 md5sum --quiet -c /var/lib/dpkg/info/"${PKG}":amd64.md5sums 2>&1)"
-    else
-        result="$(nice -n 19 ionice -c 3 md5sum --quiet -c /var/lib/dpkg/info/"${PKG}".md5sums 2>&1)"
-    fi
-
-    test $? -eq 0 && return 0
-
-    #exclude packages for which we cannot verify md5sum, no md5sums file
-    if [ "$(echo "$result" | grep md5sums | grep -c 'No such file')" -gt 0 ]
-    then
-        return 3
-    fi
-
-    #exclude ./etc (elasticsearch packaging issue)
-    result="$(echo "$result" | grep -v '^\./etc/')"
-
-    #exclude .pyc files
-    result="$(echo "$result" | grep -v '\.pyc$')"
-
-    #exclude bad formatted / empty md5sum files and md5sum summary lines
-    result="$(echo "$result" | grep -v 'did NOT match\|md5sum: ')"
-    if [ -n "$result" ]
-    then
+    RESULT=$(nice -n 19 ionice -c 3 dpkg -V "${PKG}")
+    [ $? -eq 0 ] ||
+        return 2
+    [ -n "${RESULT}" ]  &&
         return 1
-    fi
-    return 2
+    return 0
+}
+
+RET=0
+set_return_code()
+{
+    NEW_RET=$1
+    [ -z "${NEW_RET}" ] && return 0
+    (( "${NEW_RET}" > "${RET}")) && { RET="${NEW_RET}"; }
 }
 
 # Get list of all installed packages and check md5 sum for them
-RET=0
 CUSTOMIZED_PKGS=""
 ALL_PKGS=$(dpkg-query -W -f='${Package}\n') || exit 2
-for PKG in ${ALL_PKGS}; do
-    # We work only with python packages
-    PKG=$(echo "${PKG}" | grep "python-") || continue
+# We work only with python packages
+PYTHON_PKGS=$(echo "${ALL_PKGS}" | grep "python-")
 
-    PKG_POLICY=$(apt-cache -c "${APT_CONF}" policy "${PKG}") || exit 2
-    echo "${PKG_POLICY}" | grep -F "***" -A1 | grep Packages &> /dev/null
-    if [ $? != 0 ]; then
-        echo "Unidentified package: \"${PKG}\" was installed not from the configured repositories."
-        RET=1
-        continue
-    fi
-
+for PKG in ${PYTHON_PKGS}; do
     md5_verify "${PKG}"
-    if (( $? == 1 )); then
-        [[ "${CUSTOMIZED_PKGS}" != "" ]] &&
-            CUSTOMIZED_PKGS="${CUSTOMIZED_PKGS}\n"
-        CUSTOMIZED_PKGS="${CUSTOMIZED_PKGS}${PKG}"
-    fi
+    case $? in
+        0)
+            set_return_code 0
+            ;;
+        1)
+            [[ "${CUSTOMIZED_PKGS}" != "" ]] &&
+                CUSTOMIZED_PKGS+="\n"
+            CUSTOMIZED_PKGS+="${PKG}"
+
+            PKG_POLICY=$(apt-cache -c "${APT_CONF}" policy "${PKG}") || exit 2
+            echo "${PKG_POLICY}" | grep -F "***" -A1 | grep Packages &> /dev/null
+            if [ $? != 0 ]; then
+                echo "Unidentified package: \"${PKG}\" was installed not from the configured repositories."
+                set_return_code 1
+            fi
+            ;;
+        *)
+            set_return_code 2
+            ;;
+    esac
 done
-
-# Divide them on python and non-python packages
-#PYTHON_CUSTOMIZED_PKGS=$(echo -e "${CUSTOMIZED_PKGS}" | grep "python-")
-#NONPYTHON_CUSTOMIZED_PKGS=$(echo -e "${CUSTOMIZED_PKGS}" | grep -v "python-")
-
-#echo -e "${PYTHON_CUSTOMIZED_PKGS}"
-#echo -e "${NONPYTHON_CUSTOMIZED_PKGS}" &>2
 
 [ -z "${CUSTOMIZED_PKGS}" ] || echo -e "${CUSTOMIZED_PKGS}"
 
