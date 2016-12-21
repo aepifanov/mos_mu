a6ab98b3aadaf962165c72bc400324cb26cb4d3a
Andrey Epifanov <aepifanov@mirantis.com>
2016-12-21 23:50:58 +0000

    Add APT update to get_current_mu and info for undefined release

--- playbooks/files/get_current_mu.sh
+++ playbooks/files/get_current_mu.sh
@@ -7,9 +7,15 @@ APT_CONF=${APT_CONF:-"/root/mos_mu/apt/apt.conf"}
 APT_SRC_DIR=${2:-$APT_SRC_DIR}
 APT_SRC_DIR=${APT_SRC_DIR:-"/root/mos_mu/apt/sources.list.d"}
 
+OUTS="\n"
 REPOS=$(find "${APT_SRC_DIR}" -type f -name "*.list")
 for REPO in ${REPOS}; do
-	apt-get  -c "${APT_CONF}" -o Dir::Etc::sourcelist="${REPO}"  -o Dir::Etc::sourceparts="-"  --just-print upgrade | grep Inst &>/dev/null ||
+    OUT=$(apt-get  -c "${APT_CONF}" -o Dir::Etc::sourcelist="${REPO}"  -o Dir::Etc::sourceparts="-"  --just-print upgrade)
+    REPO=${REPO##*/}
+    REPO=${REPO%%.list}
+    OUTS+="${REPO}:\n"
+    OUTS+="    $(echo -e "${OUT}" | grep "upgraded,")\n"
+    echo "${OUT}" | grep Inst &>/dev/null ||
 		{ REL="${REPO}";
 		break; };
 done
@@ -17,4 +23,8 @@ done
 REL=${REL##*/}
 REL=${REL%%.list}
 
+
 echo "${REL}"
+if [[ "${REL}" == "Undefined" ]]; then
+    echo -e "${OUTS}"
+fi
