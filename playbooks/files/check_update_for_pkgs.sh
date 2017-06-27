b0549640631a24482d5f08a33ee6d264c8c9ed93
Andrey Epifanov <aepifanov@mirantis.com>
2017-06-27 07:56:51 +0000

    Improve of pacemaker update

---
+++ playbooks/files/check_update_for_pkgs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+APT_CONF=${1:-$APT_CONF}
+APT_CONF=${APT_CONF:-"/root/mos_mu/apt/apt.conf"}
+APT_UPGRADE=${2:-$APT_UPGRADE}
+APT_UPGRADE=${APT_UPGRADE:?"APT_UPGRADE is not defined"}
+PKGS=${PKGS:-$3}
+PKGS=${PKGS:?"Please specify list of packages"}
+
+
+PKGS=$(echo $PKGS | sed s/u\'//g | tr -d "[],'")
+
+RES=$(apt-get -c "${APT_CONF}" "${APT_UPGRADE}" --just-print | grep "Inst")
+
+RET=1
+OUT=""
+
+for PKG in ${PKGS}; do
+    echo "${RES}" | fgrep ${PKG}  &>/dev/null && {
+        (( RET = 0 ));
+        OUT+="${PKG}"; }
+done
+
+echo "${OUT}"
+exit ${RET}
